6cc13a62a2e1b6b1d46d229136dc0175ef5c3161
Brian Kaufman <bpkaufman34@gmail.com>
2024-08-14 20:45:09 -0500

    jaws

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     const templateData = {}
-    res.render('homepage', templateData);
+    res.render('home', templateData);
 });
 
 module.exports = router;
